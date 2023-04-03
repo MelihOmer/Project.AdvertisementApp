dfe138017606b3c3b730b10d7113c1d556eeeb92
Melih Omer <Melih Omer@DESKTOP-FILMLVF>
2023-04-04 00:46:37 +0300

    Add project files.

---
+++ Project.AdvertisementApp.Entities/MilitaryStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.AdvertisementApp.Entities
+{
+    public class MilitaryStatus:BaseEntity
+    {
+        public string Definiton { get; set; }
+        public List<AdvertisementAppUser> AdvertisementAppUsers { get; set; }
+    }
+}
